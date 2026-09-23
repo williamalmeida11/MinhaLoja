f0fb9cffa5bf3cd26fa6813ae16382c504e4a22c
RICARDO NASCIMENTO DA SILVA <ricardo.nsilva12@senacsp.edu.br>
2026-09-22 22:18:07 -0300

    tela de sobre parcial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 export const routes: Routes = [
-    {path: '', component:HomeComponent}
+    {path: '', component:HomeComponent},
+    {path: 'sobre', component:SobreComponent}
 ];
